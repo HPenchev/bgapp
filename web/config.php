c7edbc29fad8970b97a182245e9d1fb62a3062a6
HPenchev <hristo_penchev_86@abv.bg>
2022-02-24 09:33:55 +0200

    Changed configuration settings

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
-<?php
-   $database = "";
-   $user = "";
-   $password  = "";
-   $host = "";
+i<?php
+   $database = "bulgaria";
+   $user = "web_user";
+   $password  = "Password1";
+   $host = "db";
 ?>
